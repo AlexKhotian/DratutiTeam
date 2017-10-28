fb1ce69217e7986701c333366bb8d451ebb1faba
AlexKhotian <doctaww@gmail.com>
2017-10-28 17:13:10 +0200

    Added web backend + exec

---
+++ WebBackend/ServerRoutine.go
@@ -0,0 +1,27 @@
+package main
+
+import "net/http"
+
+// IServerRoutine starts a web handler server
+type IServerRoutine interface {
+	RunServer()
+}
+
+// ServerRoutineImpl implementation for ServerRoutine interface
+// contains handler
+type ServerRoutineImpl struct {
+	httpHandlerUtil *HTTPHandlerUtil
+}
+
+// ServerRoutineFactory creats intance of ServerRoutine
+func ServerRoutineFactory() IServerRoutine {
+	thisServer := new(ServerRoutineImpl)
+	thisServer.httpHandlerUtil = HTTPHandlerFactory()
+	return thisServer
+}
+
+// RunServer starts a web server
+func (serverImpl *ServerRoutineImpl) RunServer() {
+	http.Handle("/", serverImpl.httpHandlerUtil)
+	http.ListenAndServe(":8080", nil)
+}
